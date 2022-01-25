cc4452d3b939755f357a75e92c3dbb8db35a6095
thswhdals <thswhdals@naver.com>
2022-01-25 10:01:03 +0900

    확인

---
+++ index.php
@@ -0,0 +1,111 @@
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+
+<script>
+$(document).ready(function () {
+  $('#example').DataTable();
+});
+</script>
+
+<div id="wrap" style="width:100%; min-height: 100%; background-color: #ffffff;">
+  <div class='box bordered-box red-border'>
+      <div class='box-header top-content'>
+          <!-- <div class='title'>상세 정보</div> -->
+          <div>
+            <p>wallet info</p>
+          </div>
+
+      </div>
+      <div class="box-content">
+        <div class="box-title">
+          암호화폐 지갑주소 목록
+          새로고침
+        </div>
+        <div class="">
+
+          <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td>Garrett Winters</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>63</td>
+                <td>2011/07/25</td>
+                <td>$170,750</td>
+            </tr>
+            <tr>
+                <td>Ashton Cox</td>
+                <td>Junior Technical Author</td>
+                <td>San Francisco</td>
+                <td>66</td>
+                <td>2009/01/12</td>
+                <td>$86,000</td>
+            </tr>
+            <tr>
+                <td>Cedric Kelly</td>
+                <td>Senior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>2012/03/29</td>
+                <td>$433,060</td>
+            </tr>
+            <tr>
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>33</td>
+                <td>2008/11/28</td>
+                <td>$162,700</td>
+            </tr>
+          </tbody>
+          <tfoot>
+              <tr>
+                  <th>Name</th>
+                  <th>Position</th>
+                  <th>Office</th>
+                  <th>Age</th>
+                  <th>Start date</th>
+                  <th>Salary</th>
+              </tr>
+          </tfoot>
+      </table>
+
+
+
+
+        </div>
+
+      </div>
+  </div>
+</div>  <!-- /wrap -->
+
+
+<script>
+    // $(document).ready(function() {
+    //     $('body').css('background', '#F5F5F5');
+    // });
+</script>
